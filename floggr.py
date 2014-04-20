27ec1f78017f5c6a85478e5c02a9c7cec64a9e5e
Andrea Grandi <a.grandi@gmail.com>
2014-04-21 00:05:46 +0100

    Add init_db method to create database

--- floggr.py
+++ floggr.py
@@ -24,6 +24,13 @@ def connect_db():
     rv.row_factory = sqlite3.Row
     return rv
 
+def init_db():
+    with app.app_context():
+        db = get_db()
+        with app.open_resource('schema.sql', mode='r') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
+
 def get_db():
     """Opens a new database connection if there is none yet for the
     current application context.
